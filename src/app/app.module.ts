89e97014bd27e50f28c5ce13ee79208cfe4a90bb
MUD\G982829 <marco.wagener@seb.sanlam.com>
2017-06-02 10:19:58 +0200

    Added save, update and delete functions.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //ngModel
-// import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -16,7 +19,9 @@ import { HeroService } from './hero.service';
   imports: [ 
   	BrowserModule,
   	FormsModule,
-  	AppRoutingModule
+  	AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
   	AppComponent,
