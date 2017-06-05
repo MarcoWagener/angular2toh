d909a799c2eaaadedd8b74a19a7ce4de663dc1d9
MUD\G982829 <marco.wagener@seb.sanlam.com>
2017-06-05 13:59:27 +0200

    Finished the toh tut.

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -16,6 +16,7 @@ var app_component_1 = require("./app.component");
 var hero_detail_component_1 = require("./hero-detail.component");
 var heroes_component_1 = require("./heroes.component");
 var dashboard_component_1 = require("./dashboard.component");
+var hero_search_component_1 = require("./hero-search.component");
 var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
@@ -35,7 +36,8 @@ AppModule = __decorate([
             app_component_1.AppComponent,
             hero_detail_component_1.HeroDetailComponent,
             heroes_component_1.HeroesComponent,
-            dashboard_component_1.DashboardComponent
+            dashboard_component_1.DashboardComponent,
+            hero_search_component_1.HeroSearchComponent
         ],
         providers: [
             hero_service_1.HeroService
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { HeroService } from './hero.service';
 
@@ -27,7 +28,8 @@ import { HeroService } from './hero.service';
   	AppComponent,
   	HeroDetailComponent,
   	HeroesComponent,
-  	DashboardComponent
+  	DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
   	HeroService
